bacdd33fc0637e1c1dbecc9f9d23c89ccd1843cd
Classican <mosabtigerlandx@gmail.com>
2023-11-15 15:39:01 +0200

    fixed betty problems

--- main_shell.c
+++ main_shell.c
@@ -12,61 +12,38 @@
 int main(int argc, char **argv)
 {
 while (1)
-{
-char *shell = "(ACshell)";
-char *command = NULL, *command_copy = NULL;
+{char *shell = "(ACshell)", *command = NULL, *command_copy = NULL, *token;
 size_t n = 0;
 ssize_t nchars_read;
 const char *delim = " \n";
-int tokens_num = 0;
-char *token;
-char *exit = "exit";
-int i;
-int j;
-int result;
+int tokens_num = 0, i;
 (void)argc;
 printf("%s $: ", shell);
 nchars_read = getline(&command, &n, stdin);
 if (nchars_read == -1)
-{
-printf("shell shutdown. . . \n");
+{printf("shell shutdown. . . \n");
 return (-1);
 }
 command_copy = malloc(sizeof(char) * nchars_read);
 if (command_copy == NULL)
-{
-perror("tsh: memory allocation ERROR!");
+{perror("tsh: memory allocation ERROR!");
 return (-1);
 }
 strcpy(command_copy, command);
 token = strtok(command, delim);
 while (token != NULL)
-{
-tokens_num++;
+{tokens_num++;
 token = strtok(NULL, delim);
 }
 tokens_num++;
 argv = malloc(sizeof(char *) * tokens_num);
 token = strtok(command_copy, delim);
 for (i = 0; token != NULL; i++)
-{
-argv[i] = malloc(sizeof(char) * strlen(token));
+{argv[i] = malloc(sizeof(char) * strlen(token));
 strcpy(argv[i], token);
 token = strtok(NULL, delim);
 }
 argv[i] = NULL;
-result = strcmp(argv[0], exit);
-if (result == 0)
-{
-printf("shell exiting. . . .\n");
-return (-1);
-}
-printf("running command ( ");
-for (j = 0; j < tokens_num - 1; j++)
-{
-printf("%s ", argv[j]);
-}
-printf(" ) . . .\n");
 execmd(argv);
 free(command);
 free(command_copy);
